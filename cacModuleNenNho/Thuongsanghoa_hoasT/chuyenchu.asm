d1ea287e7a3b68938e220e8c1c21b643a8e986a4
NBNKhoa <nguyenbuinguyenkhoa@gmail.com>
2020-11-15 10:39:54 +0700

    thêm chương trình chuyển từ chữ hoa sang thường và ngược lại

---
+++ cacModuleNenNho/Thuongsanghoa_hoasT/chuyenchu.asm
@@ -0,0 +1,24 @@
+.ORIG	x3000
+    
+    IN
+    
+    LD R5 , A_HOA
+    ADD R3,R0,R5
+    BRP CHUYEN_THUONG ; nếu dương thì chuyển từ chữ thường sang chữ HOA
+
+    ;nếu không thì chuyển từ HOA sang thường
+    LD R5 , TO_LOWER
+    ADD	R0,	R0,	R5
+    OUT
+    HALT
+
+    CHUYEN_THUONG 
+    LD R5 , TO_CAPITAL
+    ADD	R0,	R0,	R5
+    OUT
+    HALT
+
+    A_HOA .FILL	#-95
+    TO_CAPITAL .FILL	#-32
+    TO_LOWER .FILL	#32
+.END
